fcd65f09df0c79e9d56b60f6308fd0cfd146b5b4
kicbar <bartlomiejkicior@gmail.com>
2020-12-21 21:47:23 +0100

    add return cases for country functianality

--- covidapp/views.py
+++ covidapp/views.py
@@ -13,12 +13,24 @@ response = requests.request("GET", url, headers=headers).json()
 
 # Create your views here.
 def helloworldview(request):
-    if request.method == "POST":
-        selectedcountry = request.POST['selectedcountry']
-        print(selectedcountry)
     mylist = []
     numofresults = (response['results'])
     for x in range (0, numofresults):
         mylist.append(response['response'][x]['country'])
+    if request.method == "POST":
+        selectedcountry = request.POST['selectedcountry']
+        print(selectedcountry)
+        numofresults = (response['results'])
+        for x in range(0,numofresults):
+            if selectedcountry == response['response'][x]['country']:
+                new = response['response'][x]['cases']['new']
+                active = response['response'][x]['cases']['active']
+                critical = response['response'][x]['cases']['critical']
+                recovered = response['response'][x]['cases']['recovered']
+                total = response['response'][x]['cases']['total']
+                death = int(total) - int(active) - int(recovered)
+        context = {'selectedcountry': selectedcountry, 'mylist': mylist,'new': new, 'active': active, 'critical': critical, 'recovered': recovered, 'total': total, 'death': death}
+        return render(request, 'helloworld.html', context)
+    
     context = {'mylist' : mylist}
     return render(request, 'helloworld.html', context)
