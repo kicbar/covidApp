fda869e0c22cfdc361187ebc81463df33c46817a
kicbar <bartlomiejkicior@gmail.com>
2020-12-21 20:19:30 +0100

    add get country name

--- covidapp/views.py
+++ covidapp/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+import requests
+import json
+
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+    'x-rapidapi-key': "3669e80dafmshf0fe5272ec00724p116f42jsn1ed1c524f436",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers).json()
 
 # Create your views here.
 def helloworldview(request):
-    mylistitems = ['item1', 'item2', 'item3']
-    context = {'mylistitems': mylistitems}
+    #context = {'response' : response['response'][0]}
+    mylist = []
+    numofresults = (response['results'])
+    for x in range (0, numofresults):
+        #print(str(x) + ' ' +  str(response['response'][x]['country']))
+        mylist.append(response['response'][x]['country'])
+    context = {'mylist' : mylist}
     return render(request, 'helloworld.html', context)
